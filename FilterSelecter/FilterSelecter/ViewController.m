bc27d9e929f49b2be930d11a067e7cf08d0f4519
wangyanchao <ych.wang@outlook.com>
2016-11-01 21:03:50 +0800

    use delegate callback replace block. add pictures, use lazy get method to load filterView

--- FilterSelecter/FilterSelecter/ViewController.m
+++ FilterSelecter/FilterSelecter/ViewController.m
@@ -8,8 +8,9 @@
 
 #import "ViewController.h"
 #import "YCFilterModule.h"
-@interface ViewController ()
-@property (nonatomic,strong) YCFilterModule *flterView;
+@interface ViewController ()<YCFilterModuleDelegate>
+@property (nonatomic,strong) YCFilterModule *filterView;
+@property (nonatomic,copy) NSArray *lvjingFilters;
 @end
 
 @implementation ViewController
@@ -27,34 +28,88 @@
         btn;
     });
     
-    _flterView = ({
-        _flterView = [[YCFilterModule alloc] init];
-        [_flterView setSelectedCellBlcok:^(YCFiltersType type, NSUInteger index, NSString *key) {
-            NSLog(@"didClickType:%zd, index: %zd, key: %@",type,index,key);
-            [[NSUserDefaults standardUserDefaults] setInteger:index forKey:key];
-        }];
-        _flterView;
-    });
-    
 }
 
 - (void)showFilter
 {
-    NSArray *filtersArray = @[
-                       @{@"CellTitle": @"滤镜", @"userDefaultKeyName": @"LvJing"},
-                       @{@"CellTitle": @"嫩肤", @"userDefaultKeyName": @"NenFu"},
-                       @{@"CellTitle": @"大眼", @"userDefaultKeyName": @"DaYan"},
-                       @{@"CellTitle": @"瘦脸", @"userDefaultKeyName": @"ShouLian"}
-                       ];
-    NSArray *choseBarArray = @[
-                               @"lvjing",
-                               @"nenfu",
-                               @"dayan",
-                               @"shoulian",
-                               ];
-    _flterView.filtersViewData = filtersArray;
-    _flterView.choseBarData = choseBarArray;
-    [_flterView showInView:self.view.window];
+    [self.filterView showInView:self.view.window];
 }
+
+#pragma mark - YCFilterModuleDelegate
+-(void)didSelectedFiltesWithType:(YCFiltersType)type index:(NSUInteger)index forFilterNameKey:(NSString *)key
+{
+    NSLog(@"didClickType:%zd, index: %zd, key: %@",type,index,key);
+    [[NSUserDefaults standardUserDefaults] setInteger:index forKey:key];
+}
+
+
+-(YCFilterModule *)filterView
+{
+    if (!_filterView) {
+        _filterView = [[YCFilterModule alloc] init];
+        _filterView.delegate = self;
+        NSArray *filtersArray = @[
+                                  @{@"CellTitle": @"滤镜", @"userDefaultKeyName": @"LvJing",
+                                    @"sonCellTitles":@[
+                                            @"原图",
+                                            @"甜昔",
+                                            @"春光",
+                                            @"清风",
+                                            @"粉黛",
+                                            @"海滩",
+                                            @"晨曦",
+                                            @"白露",
+                                            @"嫩枝",
+                                            @"薄暮",
+                                            @"胶片",
+                                            @"橘彩",
+                                            @"魅影",
+                                            @"暖茶",
+                                            @"夜梦",
+                                            ],
+                                    @"sonCellImageNames":self.lvjingFilters,
+                                    },
+                                  @{@"CellTitle": @"嫩肤", @"userDefaultKeyName": @"NenFu"},
+                                  @{@"CellTitle": @"大眼", @"userDefaultKeyName": @"DaYan"},
+                                  @{@"CellTitle": @"瘦脸", @"userDefaultKeyName": @"ShouLian"}
+                                  ];
+        NSArray *choseBarArray = @[
+                                   @"lvjing",
+                                   @"nenfu",
+                                   @"dayan",
+                                   @"shoulian",
+                                   ];
+        _filterView.filtersViewData = filtersArray;
+        _filterView.choseBarData = choseBarArray;
+    }
+    return _filterView;
+}
+
+
+-(NSArray *)lvjingFilters
+{
+    if (!_lvjingFilters) {
+        _lvjingFilters = @[
+                           @"10000_4",
+                           @"10001_4",
+                           @"10002_4",
+                           @"10003_4",
+                           @"10004_4",
+                           @"10005_4",
+                           @"10014_3",
+                           @"10015_4",
+                           @"10016_4",
+                           @"10020_4",
+                           @"10021_4",
+                           @"10022_2",
+                           @"10023_5",
+                           @"10024_3",
+                           @"10026_2",
+                           ];
+    }
+    return _lvjingFilters;
+}
+
+
 
 @end
